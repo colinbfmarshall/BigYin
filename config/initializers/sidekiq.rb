c1c2c1dc0d7f4b4428d4f1d2fff82f496e1550ee
colinbfmarshall <colinbfmarshall@gmail.com>
2016-09-12 20:51:10 +0100

    updated the initializer/sidekiq.rb, puma.rb and sidekiq.yml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,29 +1,26 @@
-require 'sidekiq'
-require 'autoscaler/sidekiq'
-require 'autoscaler/heroku_scaler'
+if Rails.env.production?
 
-heroku = nil
-if ENV['HEROKU_APP']
-  heroku = Autoscaler::HerokuScaler.new
-end
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
+  end
 
-Sidekiq.configure_client do |config|
-  if heroku
-    config.client_middleware do |chain|
-      chain.add Autoscaler::Sidekiq::Client, 'default' => heroku
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 20 }
+
+    Rails.application.config.after_initialize do
+      Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+      ActiveRecord::Base.connection_pool.disconnect!
+
+      ActiveSupport.on_load(:active_record) do
+        config = Rails.application.config.database_configuration[Rails.env]
+        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+        # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
+        config['pool'] = 16
+        ActiveRecord::Base.establish_connection(config)
+
+        Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+      end
     end
   end
-  config.redis = { :size => 1 }
-end
 
-Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    if heroku
-      p "[Sidekiq] Running on Heroku, autoscaler is used"
-      chain.add(Autoscaler::Sidekiq::Server, heroku, 60) # 60 seconds timeout
-    else
-      p "[Sidekiq] Running locally, so autoscaler isn't used"
-    end
-  end
-  config.redis = { :size => 1 }
-end
+end  
