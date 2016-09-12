a78e0502e1c946759e8396ff9892d4f43065da36
colinbfmarshall <colinbfmarshall@gmail.com>
2016-09-12 20:33:58 +0100

    updated gemfile

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'puma_worker_killer'
 
 gem 'redis', '~>3.2'
 gem 'sidekiq'
+gem 'autoscaler'
 
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-require 'redis'
+# require 'redis'
 
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
