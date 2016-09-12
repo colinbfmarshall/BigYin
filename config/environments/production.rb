b4aacacd5dd8ccf58153ade6b9721bc4db443037
colinbfmarshall <colinbfmarshall@gmail.com>
2016-09-12 16:35:50 +0100

    updated bin/rails bin/rake and gemfile.lock

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,4 +12,14 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
+#   config.action_mailer.smtp_settings = {
+#       address: 'smtp.gmail.com',
+#       port: 587,
+#       domain: 'gmail.com',
+#       user_name: 'mygmail_id@gmail.com',
+#       password: 'mygmail_password',
+#       authentication: 'plain',
+#       enable_starttls_auto: true,
+#       :openssl_verify_mode => 'none'  # This line added and it works fine
+# }
 end
