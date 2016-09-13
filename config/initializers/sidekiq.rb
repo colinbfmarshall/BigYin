1d8ef4643fa2d1ef405bfd754ed2a9f78f84813c
colinbfmarshall <colinbfmarshall@gmail.com>
2016-09-13 16:49:50 +0100

    app is crashing on boot

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,25 +1,51 @@
-if Rails.env.production?
+# /config/initializers/sidekiq.rb
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 1 }
-  end
+current_web_concurrency = Proc.new do
+  web_concurrency = ENV['WEB_CONCURRENCY']
+  web_concurrency ||= Puma.respond_to?
+  (:cli_config) && Puma.cli_config.options.fetch(:max_threads)
+  web_concurrency || 16
+end
 
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 2  }
+local_redis_url = Proc.new do
+  config_file = File.join(File.expand_path('../..', __FILE__), 'redis.conf')
+  port = File.read(config_file).chomp.split.last
+  "redis://localhost:#{port}"
+end
 
-    Rails.application.config.after_initialize do
-      Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      ActiveRecord::Base.connection_pool.disconnect!
+ENV['REDISTOGO_URL'] ||= local_redis_url.call
 
-      ActiveSupport.on_load(:active_record) do
-        config = Rails.application.config.database_configuration[Rails.env]
-        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || 10
-        ActiveRecord::Base.establish_connection(config)
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 
-        Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      end
+  Rails.application.config.after_initialize do
+    ActiveRecord::Base.connection_pool.disconnect!
+
+    ActiveSupport.on_load(:active_record) do
+      config = Rails.application.config.database_configuration[Rails.env]
+      config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+      config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
+      ActiveRecord::Base.establish_connection(config)
+
+      Rails.logger.info("Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
     end
   end
+end
 
-end  
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDISTOGO_URL'], :size => 1 }
+
+  Rails.application.config.after_initialize do
+    ActiveRecord::Base.connection_pool.disconnect!
+
+    ActiveSupport.on_load(:active_record) do
+      config = Rails.application.config.database_configuration[Rails.env]
+      config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+      config['pool'] = ENV['WEB_DB_POOL_SIZE'] || current_web_concurrency.call
+      ActiveRecord::Base.establish_connection(config)
+
+      # DB connection not available during slug compliation on Heroku
+      Rails.logger.info("Connection Pool size for web server is now: #{config['pool']}")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
+
   mount VideoUploader::UploadEndpoint, at: "/attachments/videos"
 
   root 'welcome#index'
-
   devise_for :users
-
   resources :topics do
     resources :posts
   end
-
   resources :posts, only: [:show]
-
 end
