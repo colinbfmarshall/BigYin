b432b327e4fb657f1db3a962b0e995cc43ee3dc1
colinbfmarshall <colinbfmarshall@gmail.com>
2016-09-20 13:35:52 +0100

    updated the admin sign in and profile table

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: %i[new create]
+  before_action :authenticate_user!, only: %i[new create show edit update destroy]
+  before_action :authenticate_admin!, only: %i[index]
 
   # GET /profiles
   # GET /profiles.json
