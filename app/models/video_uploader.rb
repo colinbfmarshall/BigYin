d1ed26ecc4ea05e47776a1a75f44ba924344fd0d
colinbfmarshall <colinbfmarshall@gmail.com>
2016-09-21 15:38:23 +0100

    updated the videouploader video specs

--- app/models/video_uploader.rb
+++ app/models/video_uploader.rb
@@ -11,7 +11,7 @@ class VideoUploader < Shrine
     screenshot = Tempfile.new(["screenshot", ".jpg"], binmode: true)
 
     movie = FFMPEG::Movie.new(mov.path)
-    options = { custom: %w(-c:v libx264 -profile:v baseline -level 3.0 -t 00:00:20) } 
+    options = { custom: %w(-c:v libx264 -t 00:00:20) } 
     movie.transcode(video.path, options)
     movie.screenshot(screenshot.path, seek_time: 5)
     mov.delete
