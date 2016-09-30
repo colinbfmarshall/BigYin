ed63fddfc242cdbf205018b76ffe848216f39138
colinbfmarshall <colinbfmarshall@gmail.com>
2016-09-30 14:54:41 +0100

    updated the shrine.rb

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,25 +2,25 @@ require "shrine"
 require "shrine/storage/s3"
 require "shrine/storage/url"
 
-Shrine.storages = {
-  cache: Shrine::Storage::Url.new,
-  store: Shrine::Storage::S3.new(
-    bucket:            ENV.fetch("S3_BUCKET"),
-    region:            ENV.fetch("S3_REGION"),
-    access_key_id:     ENV.fetch("S3_ACCESS_KEY_ID"),
-    secret_access_key: ENV.fetch("S3_SECRET_ACCESS_KEY"),
-  ),
+s3_options = {
+  access_key_id:     ENV.fetch("S3_ACCESS_KEY_ID"),
+  secret_access_key: ENV.fetch("S3_SECRET_ACCESS_KEY"),
+  region:            ENV.fetch("S3_REGION"),
+  bucket:            ENV.fetch("S3_BUCKET"),
 }
 
-Shrine.plugin :transloadit,
-  auth_key: ENV.fetch("TRANSLOADIT_KEY"),
-  auth_secret: ENV.fetch("TRANSLOADIT_SECRET")
+Shrine.storages = {
+  cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+  store: Shrine::Storage::S3.new(prefix: "store", **s3_options),
+}
 
 Shrine.plugin :activerecord
 Shrine.plugin :logging, logger: Rails.logger
 Shrine.plugin :direct_upload, presign: true
 Shrine.plugin :backgrounding
+Shrine.plugin :transloadit,
+  auth_key: ENV.fetch("TRANSLOADIT_KEY"),
+  auth_secret: ENV.fetch("TRANSLOADIT_SECRET")
 
-
-Shrine::Attacher.promote { |data| TransloaditJob.perform_async(data) }
+Shrine::Attacher.promote { |data| PromoteJob.perform_async(data) }
 Shrine::Attacher.delete { |data| DeleteJob.perform_async(data) }
