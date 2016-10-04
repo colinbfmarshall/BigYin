fa00ecde33b2c1d60e922d051e5c22fd14ebc318
colinbfmarshall <colinbfmarshall@gmail.com>
2016-10-04 13:50:42 +0100

    updated the video uploader again, working in the development env

--- app/models/video_uploader.rb
+++ app/models/video_uploader.rb
@@ -8,7 +8,15 @@ class VideoUploader < Shrine
     video = original.add_step("encoding", "/video/encode", preset: "iphone", ffmpeg: { t: 20})
     screenshot = original.add_step("thumbnail", "/video/thumbs", count: 1)
     files = {video: video, screenshot: screenshot}
-    transloadit_assembly(files, context: context)
+
+    if ENV["RACK_ENV"] == "production"
+      notify_url = "https://infinite-reaches-64522.herokuapp.com/webhooks/transloadit"
+    else
+      # In development we cannot receive webhooks, because Transloadit as an
+      # external service cannot reach our localhost.
+    end
+
+    transloadit_assembly(files, context: context, notify_url: notify_url )
   end
 
   # plugin :processing
