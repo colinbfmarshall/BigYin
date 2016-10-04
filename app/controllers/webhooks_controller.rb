77c7db7fa7b4648afe3bab22988197b7c08504d5
colinbfmarshall <colinbfmarshall@gmail.com>
2016-10-04 15:37:00 +0100

    removed the transloadit method from the webhooks controller

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,7 +1,4 @@
 class WebhooksController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:transloadit]
-
-  def transloadit
-  end
+  skip_before_action :verify_authenticity_token
 
 end
