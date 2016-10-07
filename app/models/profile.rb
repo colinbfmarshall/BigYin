5c60d6192f308b314f088f7a9fdff0f8270324c8
colinbfmarshall <colinbfmarshall@gmail.com>
2016-10-07 20:40:08 +0100

    updated the profile page too

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,8 +3,9 @@ class Profile < ActiveRecord::Base
 
   validates_presence_of :first_name
   validates_presence_of :age
-  validates_presence_of :location
-  validates_presence_of :occupation
+  validates_presence_of :nationality
+  # validates_presence_of :location
+  # validates_presence_of :occupation
   # validates_presence_of :gender
   # validates_presence_of :marital_status
 
